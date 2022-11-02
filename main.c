fe6ba563fc4caf845e38376a228475c2f817dc61
Samuel Jakobsen <samuel@skaarhoj.com>
2022-11-02 14:21:34 +0100

    Smarter stupid multithreading...

--- main.c
+++ main.c
@@ -12,6 +12,7 @@
 typedef struct {
     pthread_rwlock_t *lock;
     pthread_t *tid;
+    int i;
 } arg_struct;
 
 pthread_rwlock_t lock_rw = PTHREAD_RWLOCK_INITIALIZER;
@@ -68,13 +69,13 @@ int checkSudoku(void *vargp) {
     pthread_rwlock_t *p = arg->lock;
     char localSudoku[9][9];
 
-    if (pthread_rwlock_rdlock(p) != 0) {
+        if (pthread_rwlock_rdlock(p) != 0) {
         perror("reader_threadC: pthread_rwlock_rdlock error");
         exit(__LINE__);
     }
     memcpy(&localSudoku, &sudoku, sizeof (char) * 9 * 9);
     if (pthread_rwlock_unlock(p) != 0) {
-        perror("reader thread: pthred_rwlock_unlock error");
+        perror("reader threadC: pthred_rwlock_unlock error");
         exit(__LINE__);
     }
 
@@ -152,7 +153,7 @@ int checkBoxFromPos(int number, int i, int j, char sudoku[9][9]) {
 }
 
 
-int brute(int start, int myid, pthread_rwlock_t *p) {
+int brute(int number, int myid, pthread_rwlock_t *p) {
     char localSudoku[9][9];
 
     if (pthread_rwlock_rdlock(p) != 0) {
@@ -161,13 +162,13 @@ int brute(int start, int myid, pthread_rwlock_t *p) {
     }
     memcpy(&localSudoku, &sudoku, sizeof (char) * 9 * 9);
     if (pthread_rwlock_unlock(p) != 0) {
-        perror("reader thread: pthred_rwlock_unlock error");
+        perror("reader threadB: pthred_rwlock_unlock error");
         exit(__LINE__);
     }
 
     int found = 0;
-    for (int round = 0; round < 9; ++round) {
-        int number = ((round+start-1)%9)+1;
+    /*for (int round = 0; round < 9; ++round) {
+        int number = ((round+start-1)%9)+1;*/
         //printf("%d, ",number);
 
         for (int i = 0; i < 9; ++i) {
@@ -179,6 +180,7 @@ int brute(int start, int myid, pthread_rwlock_t *p) {
 
 
             for (int j = 0; j < 9; ++j) {
+                Sleep(25);
                 if (sudoku[i][j]) continue;
 
                 //if ((i%3 == 0 ) && (j%3 == 0 )) {
@@ -212,19 +214,18 @@ int brute(int start, int myid, pthread_rwlock_t *p) {
             }
 
         }
-    }
+    //}
     return found;
 }
 
 void *runBrute(void *vargp) {
     arg_struct *arg = vargp;
     int *myid = (int *)arg->tid;
-    int start = abs(*myid)%9;
 
-    printf("Thread ID: %d, Start: %d\n", *myid, start);
+    printf("Thread ID: %d, Start: %d\n", *myid, arg->i);
 
     while (1){
-        brute(start+1, *myid, arg->lock);
+        brute(arg->i, *myid, arg->lock);
     };
 }
 
@@ -239,8 +240,9 @@ void *runCheck(void *vargp) {
 int main() {
     printSudoku();
 
-    int i;
-    pthread_t checkTid, tid1, tid2;
+    pthread_t checkTid;
+    pthread_t tid[9];
+
     void *vp;
 
     arg_struct arg1;
@@ -251,46 +253,40 @@ int main() {
         exit (__LINE__);
     }
 
-
-    arg_struct arg2;
-    arg2.lock = &lock_rw;
-    arg2.tid = &tid1;
-    if (pthread_create(&tid1, NULL, runBrute, &arg2) != 0) {
-        perror("pthread_create error");
-        exit (__LINE__);
+    /*
+    if (pthread_rwlock_wrlock(&lock_rw) != 0) {
+        perror("writer thread: pthread_rwlock_wrlock error");
+        exit(__LINE__);
     }
+     */
 
-    Sleep(25);
-    arg_struct arg3;
-    arg3.lock = &lock_rw;
-    arg3.tid = &tid2;
-    if (pthread_create(&tid2, NULL, runBrute, &arg3) != 0) {
-        perror("pthread_create error");
-        exit (__LINE__);
+    arg_struct arg[9];
+    for (int j = 0; j < 9; ++j) {
+        arg[j].lock = &lock_rw;
+        arg[j].i = j+1;
+        arg[j].tid = &tid[j];
+        if (pthread_create(&tid[j], NULL, runBrute, &arg[j]) != 0) {
+            perror("pthread_create error");
+            exit (__LINE__);
+        }
     }
-
 
     printf("Waiting...\n");
+
+    /*if (pthread_rwlock_unlock(&lock_rw) != 0) {
+        perror("writer thread: pthread_rwlock_wrlock error");
+        exit(__LINE__);
+    }*/
 
     //wait for the thread to complete
     if (pthread_join(checkTid, &vp) != 0) {
         perror("pthread_join error");
         exit (__LINE__);
     }
-    pthread_cancel(tid1);
-    pthread_cancel(tid2);
+    for (int i = 0; i < 9; ++i) {
+        pthread_cancel(tid[i]);
+    }
     printf("Done!");
-
-    //wait for the thread to complete
-    if (pthread_join(tid1, &vp) != 0) {
-        perror("pthread_join error");
-        exit (__LINE__);
-    }
-
-    if (pthread_join(tid2, &vp) != 0) {
-        perror("pthread_join error");
-        exit (__LINE__);
-    }
 
     //while (brute(4));
 
@@ -303,6 +299,6 @@ int main() {
     printf("Done? %s\n", checkSudoku(&arg4) ? "Yes" : "No");
 
 
-    pthread_exit(NULL);
+   // pthread_exit(NULL);
     return 0;
 }
