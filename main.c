03d2e61075ee769783c0a2de42b98f11ab5e1220
Samuel Jakobsen <samuel@skaarhoj.com>
2022-11-02 12:16:14 +0100

    Works?

--- main.c
+++ main.c
@@ -14,6 +14,16 @@ int sudoku[9][9] = {{4, 0, 1, 8, 0, 0, 0, 0, 0},
                     {0, 0, 0, 5, 1, 6, 8, 0, 0},
                     {0, 0, 4, 0, 0, 0, 0, 0, 9},};
 
+int sudoku2[9][9] = {{0, 0, 0, 0, 0, 0, 1, 0, 0},
+                     {0, 2, 0, 3, 0, 4, 9, 0, 0},
+                     {9, 0, 5, 7, 0, 0, 6, 0, 8},
+                     {3, 7, 2, 0, 0, 5, 1, 8, 0},
+                     {0, 0, 0, 0, 3, 0, 0, 9, 0},
+                     {0, 1, 0, 6, 0, 7, 0, 2, 0},
+                     {0, 8, 7, 9, 0, 0, 2, 6, 0},
+                     {0, 0, 0, 5, 1, 6, 8, 0, 0},
+                     {0, 0, 4, 0, 0, 0, 0, 0, 9},};
+
 
 int finalSudoku[9][9] = {{4, 6, 1, 8, 5, 9, 7, 3, 2},
                          {7, 2, 8, 3, 6, 4, 9, 5, 1},
@@ -97,11 +107,14 @@ int checkBox(int number, int box) {
 
 // checkBoxFromPos goes through the box and checks if the number is present. Returns true if found. Box ranges from 1-9.
 int checkBoxFromPos(int number, int i, int j) {
-    int offsetV = i / 3;
-    int offsetH = j / 3;
+    int offsetV = j / 3;
+    int offsetH = i / 3;
+
+    //printf("i: %d, j: %d, offv: %d, offh: %d\n", i, j, offsetV, offsetH);
 
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
+
             if (sudoku[i + offsetH * 3][j + offsetV * 3] == number) {
                 return 1;
             }
@@ -110,42 +123,47 @@ int checkBoxFromPos(int number, int i, int j) {
     return 0;
 }
 
-void brute() {
+int brute() {
+    int found = 0;
     for (int number = 1; number <= 9; ++number) {
-        printf("\ntrying number: %d -\t", number);
+
         for (int i = 0; i < 9; ++i) {
-            printf("trying i: %d- \t", i);
             if (checkColumn(number, i)) {
                 continue;
             }
             int potentialPositions = 0;
             int position = 0;
 
+
             for (int j = 0; j < 9; ++j) {
-                if ((i%3 == 0 ) && (j%3 == 0 )) {
+                if (sudoku[i][j]) continue;
+
+                //if ((i%3 == 0 ) && (j%3 == 0 )) {
                     if(checkBoxFromPos(number, i, j)) {
                         continue; //TODO: do more here?
                     }
-                }
+                //}
                 if (!checkLine(number, j)) {
                     potentialPositions++;
                     position = j;
                 }
             }
+
             if (potentialPositions == 1) {
+                if (sudoku[i][position]) continue;
                 sudoku[i][position] = number;
-                printf("Put in number: %d, at i: %d, j: %d", number, i, position);
+                printf("Put in number: %d, at i: %d, j: %d\n", number, i, position);
+                found++;
             }
-
         }
-
     }
+    return found;
 }
 
 
 int main() {
     printSudoku();
-    brute();
+    while (brute());
     printf("\n\n");
     printSudoku();
     printf("Done? %s", checkSudoku() ? "Yes" : "No");
